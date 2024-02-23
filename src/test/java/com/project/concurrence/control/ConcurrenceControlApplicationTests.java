7b0e991bdd82f874ced815195dadf2823ed81110
Maxel-Uds <maxellopes32@gmail.com>
2024-02-22 23:00:44 -0300

    Mudando config de banco

--- src/test/java/com/project/concurrence/control/ConcurrenceControlApplicationTests.java
+++ src/test/java/com/project/concurrence/control/ConcurrenceControlApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConcurrenceControlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
