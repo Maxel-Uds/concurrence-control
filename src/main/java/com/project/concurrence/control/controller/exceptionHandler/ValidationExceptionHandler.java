07d121c94d6642a67716e848fd68ed5ce32b58f4
Maxel-Uds <maxellopes32@gmail.com>
2024-05-22 23:16:07 -0300

    Ajustando tratamento de erros

--- src/main/java/com/project/concurrence/control/controller/exceptionHandler/ValidationExceptionHandler.java
+++ src/main/java/com/project/concurrence/control/controller/exceptionHandler/ValidationExceptionHandler.java
@@ -4,24 +4,25 @@ import com.project.concurrence.control.exception.InvalidAmountTransactionExcepti
 import com.project.concurrence.control.exception.InvalidEnumTypeException;
 import com.project.concurrence.control.exception.InvalidTransactionException;
 import com.project.concurrence.control.exception.UserNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.bind.support.WebExchangeBindException;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
-@RestControllerAdvice
+@ControllerAdvice
 public class ValidationExceptionHandler {
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    @ExceptionHandler(WebExchangeBindException.class)
-    public ErrorResponse validationExceptionHandler(WebExchangeBindException exception){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validationExceptionHandler(MethodArgumentNotValidException exception){
 
         final List<FieldMessage> errors =
                 exception.getFieldErrors()
@@ -34,84 +35,91 @@ public class ValidationExceptionHandler {
                         .collect(Collectors.toList());
 
         log.error("==== Input error ====");
-        return ValidationErrorResponse
+        return ResponseEntity.unprocessableEntity().body(
+                ValidationErrorResponse
                 .builder()
                 .errors(errors)
                 .timestamp(System.currentTimeMillis())
                 .message("Input validation error")
                 .status(HttpStatus.UNPROCESSABLE_ENTITY.value())
-                .build();
+                .build()
+        );
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidAmountTransactionException.class)
-    public ErrorResponse invalidAmountTransactionException(InvalidAmountTransactionException exception, ServerHttpRequest request){
-        return invalidAmountTransactionResponseOf(exception, request);
+    public ResponseEntity<ErrorResponse> invalidAmountTransactionException(InvalidAmountTransactionException exception, HttpServletRequest request){
+        return ResponseEntity.unprocessableEntity().body(invalidAmountTransactionResponseOf(exception, request));
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidEnumTypeException.class)
-    public ErrorResponse invalidTypeEnumExceptionHandler(InvalidEnumTypeException exception, ServerHttpRequest request){
-        return invalidEnumTypeResponseOf(exception, request);
+    public ResponseEntity<ErrorResponse> invalidTypeEnumExceptionHandler(InvalidEnumTypeException exception, HttpServletRequest request){
+        return ResponseEntity.unprocessableEntity().body(invalidEnumTypeResponseOf(exception, request));
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
-    public ErrorResponse resourceNotFoundException(UserNotFoundException exception, ServerHttpRequest request){
-        return resourceNotFoundRequestResponseOf(exception, request);
+    public ResponseEntity<ErrorResponse> resourceNotFoundException(UserNotFoundException exception, HttpServletRequest request){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(resourceNotFoundRequestResponseOf(exception, request));
     }
 
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(InvalidTransactionException.class)
-    public ErrorResponse invalidTransactionException(InvalidTransactionException exception, ServerHttpRequest request){
-        return invalidTransactionResponseOf(exception, request);
+    public ResponseEntity<ErrorResponse> invalidTransactionException(InvalidTransactionException exception, HttpServletRequest request){
+        return ResponseEntity.unprocessableEntity().body(invalidTransactionResponseOf(exception, request));
     }
 
-    private ErrorResponse invalidTransactionResponseOf(InvalidTransactionException exception, ServerHttpRequest request){
-        log.error("==== Error: [{}]. Path: [{}]. Method: [{}]. Code: [{}] ====", exception.getMessage(), request.getURI().getPath(), request.getMethod(), exception.getCode());
+    private ErrorResponse invalidTransactionResponseOf(InvalidTransactionException exception, HttpServletRequest request){
+        this.logRequest(exception.getMessage(), request.getRequestURI(), request.getMethod(), exception.getCode());
         return ErrorResponse
                 .builder()
                 .message(exception.getMessage())
                 .status(exception.getCode())
                 .timestamp(System.currentTimeMillis())
-                .path(request.getURI().getPath())
+                .path(request.getRequestURI())
                 .error(exception.getStatus().name().toLowerCase())
                 .build();
     }
 
-    private ErrorResponse resourceNotFoundRequestResponseOf(UserNotFoundException exception, ServerHttpRequest request){
-        log.error("==== Error: [{}]. Path: [{}]. Method: [{}]. Code: [{}] ====", exception.getMessage(), request.getURI().getPath(), request.getMethod(), exception.getCode());
+    private ErrorResponse resourceNotFoundRequestResponseOf(UserNotFoundException exception, HttpServletRequest request){
+        this.logRequest(exception.getMessage(), request.getRequestURI(), request.getMethod(), exception.getCode());
         return ErrorResponse
                 .builder()
                 .message(exception.getMessage())
                 .status(exception.getCode())
                 .timestamp(System.currentTimeMillis())
-                .path(request.getURI().getPath())
+                .path(request.getRequestURI())
                 .error(exception.getStatus().name().toLowerCase())
                 .build();
     }
 
-    private ErrorResponse invalidEnumTypeResponseOf(InvalidEnumTypeException exception, ServerHttpRequest request) {
-        log.info("==== Error: [{}]. Path: [{}]. Method: [{}]. Code: [{}] ====", exception.getMessage(), request.getURI().getPath(), request.getMethod(), exception.getCode());
+    private ErrorResponse invalidEnumTypeResponseOf(InvalidEnumTypeException exception, HttpServletRequest request) {
+        this.logRequest(exception.getMessage(), request.getRequestURI(), request.getMethod(), exception.getCode());
         return ErrorResponse
                 .builder()
                 .message(exception.getMessage())
                 .status(exception.getCode())
                 .timestamp(System.currentTimeMillis())
-                .path(request.getURI().getPath())
+                .path(request.getRequestURI())
                 .error(exception.getStatus().name().toLowerCase())
                 .build();
     }
 
-    private ErrorResponse invalidAmountTransactionResponseOf(InvalidAmountTransactionException exception, ServerHttpRequest request){
-        log.error("==== Error: [{}]. Path: [{}]. Method: [{}]. Code: [{}] ====", exception.getMessage(), request.getURI().getPath(), request.getMethod(), exception.getCode());
+    private ErrorResponse invalidAmountTransactionResponseOf(InvalidAmountTransactionException exception, HttpServletRequest request){
+        this.logRequest(exception.getMessage(), request.getRequestURI(), request.getMethod(), exception.getCode());
         return ErrorResponse
                 .builder()
                 .message(exception.getMessage())
                 .status(exception.getCode())
                 .timestamp(System.currentTimeMillis())
-                .path(request.getURI().getPath())
+                .path(request.getRequestURI())
                 .error(exception.getStatus().name().toLowerCase())
                 .build();
+    }
+
+    private void logRequest(String error, String path, String method, int code) {
+        log.error("==== Error: [{}]. Path: [{}]. Method: [{}]. Code: [{}] ====", error, path, method, code);
+
     }
 }
